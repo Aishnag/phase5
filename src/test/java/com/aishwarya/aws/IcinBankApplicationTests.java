4f3f2d031afea3f331a78456579872a5cd95ba95
Aishwarya <aishnagare12@gmail.com>
2022-03-17 11:23:36 +0530

    first commit.

---
+++ src/test/java/com/aishwarya/aws/IcinBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aishwarya.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcinBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
